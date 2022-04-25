58d092a39585c6c15005464e843cc4c2b8d2558a
roaa94 <roaa94khaddam@gmail.com>
2022-04-25 08:14:50 +0300

    add comments

--- lib/widgets/cool_toolbar.dart
+++ lib/widgets/cool_toolbar.dart
@@ -11,10 +11,27 @@ class CoolToolbar extends StatefulWidget {
 }
 
 class _CoolToolbarState extends State<CoolToolbar> {
+  /// Scroll controller to be hooked with the items list
   late ScrollController scrollController;
 
   double get itemHeight =>
       Constants.toolbarWidth - (Constants.toolbarHorizontalPadding * 2);
+
+  /// List of item scales
+  ///
+  /// item at index [x] has scale of [itemScrollScaleValues[x]]
+  List<double> itemScrollScaleValues = [];
+
+  /// List of item y positions
+  ///
+  /// item at index [x] has y position of [itemYPositions[x]]
+  /// The position is of the top edge of each item and is local to the toolbar container
+  List<double> itemYPositions = [];
+
+  /// List of bool values indicating long press
+  ///
+  /// item at index [x] is long pressed id [itemYPositions[x]] is true
+  List<bool> longPressedItemsFlags = [];
 
   void scrollListener() {
     if (scrollController.hasClients) {
@@ -24,22 +41,39 @@ class _CoolToolbarState extends State<CoolToolbar> {
     }
   }
 
-  List<double> itemScrollScaleValues = [];
-  List<double> itemYPositions = [];
-
+  /// Updates scale values and item positions
+  /// based on the current scroll position
+  ///
+  /// Called inside scrollListener to update [itemScrollScaleValues] & [itemYPositions]
   void _updateItemsScrollData({double scrollPosition = 0}) {
     List<double> _itemScrollScaleValues = [];
     List<double> _itemYPositions = [];
     for (int i = 0; i <= toolbarItems.length - 1; i++) {
       double itemTopPosition = i * (itemHeight + Constants.itemsGutter);
+      // Storing y position values of the items with the scrollPosition value subtracted
+      // gives the location of the item relative to the toolbar container
+      // For example, at first, item 1 is at the top with a position of 0,
+      // but when scrolled a distance of 20, item 2's position is now it's previous position
+      // (let's say 20) minus the scrolled distance, making it at the correct position when
+      // a long press event is received at this location
       _itemYPositions.add(itemTopPosition - scrollPosition);
 
-      double itemBottomPosition =
-          (i + 1) * (itemHeight + Constants.itemsGutter);
+      // Difference between the position of the top edge of the item
+      // and the position of the bottom edge of the toolbar container plus scroll position
+      // A negative value means that the item is out of view below the toolbar container
       double distanceToMaxScrollExtent =
           Constants.toolbarHeight + scrollPosition - itemTopPosition;
+
+      // Position of the bottom edge of the item
+      double itemBottomPosition =
+          (i + 1) * (itemHeight + Constants.itemsGutter);
+      // An item is out of view if it's out of view below the toolbar
+      // OR if it's out of view above the toolbar (where the scroll position is further than
+      // the position of the bottom edge of the item
       bool itemIsOutOfView =
           distanceToMaxScrollExtent < 0 || scrollPosition > itemBottomPosition;
+
+      // If the item is out of view, scale it down, if it's visible, reset it to default scale
       _itemScrollScaleValues.add(itemIsOutOfView ? 0.4 : 1);
     }
     setState(() {
@@ -48,11 +82,17 @@ class _CoolToolbarState extends State<CoolToolbar> {
     });
   }
 
-  List<bool> longPressedItemsFlags = [];
-
+  /// Updates [longPressedItemsFlags] based on the location of the long press event
+  ///
+  /// Should be called whenever a long-press related event happens
+  /// e.g. Long press started, ended, cancelled, moved while pressed, ...etc.
   void _updateLongPressedItemsFlags({double longPressYLocation = 0}) {
     List<bool> _longPressedItemsFlags = [];
     for (int i = 0; i <= toolbarItems.length - 1; i++) {
+      // An item is long pressed if the long press y location is larger than the current
+      // item's top edge position and smaller than the next items top edge position
+      // If there is no next item, the long press y location should be smaller
+      // than the scrollable area (the height of the toolbar_
       bool isLongPressed = itemYPositions[i] >= 0 &&
           longPressYLocation > itemYPositions[i] &&
           longPressYLocation <
@@ -123,7 +163,6 @@ class _CoolToolbarState extends State<CoolToolbar> {
             },
             child: ListView.builder(
               controller: scrollController,
-              // clipBehavior: Clip.none,
               padding: const EdgeInsets.all(10),
               itemCount: toolbarItems.length,
               itemBuilder: (c, i) => ToolbarItem(
